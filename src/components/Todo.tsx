8998a11432b3c69848e7c6821ead4d2f39d57e7e
Cauê Amaral <caue.amaral@hotmail.com>
2025-06-17 22:16:07 -0300

    style: form layout

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -6,17 +6,25 @@ export default function Todo() {
     const setInput = useTodoStore((state) => state.setInput)
 
     return (
-        <section className="bg-white mt-10 max-w-sm mx-auto p-3 rounded-md shadow-lg">
-            <h1 className="text-center font-bold text-2xl m">
+        <section className="bg-white mt-10 max-w-sm mx-auto p-5 rounded-md shadow-lg">
+            <h1 className="text-center font-bold text-2xl mb-5">
                 Todo List
             </h1>
-            <input
-                type="text"
-                placeholder="Add Todo"
-                value={input}
-                onChange={(e) => setInput(e.target.value)}
-                onKeyDown={(e) => e.key === 'Enter' && addTodo(input)}
-            />
+            <form className="flex">
+                <input
+                    type="text"
+                    placeholder="Add Todo"
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                    onKeyDown={(e) => e.key === 'Enter' && addTodo(input)}
+                    className="border border-gray-300 flex-grow py-1 px-2"
+                />
+                <button
+                    className="bg-blue-500 text-white px-4 rounded-r-md cursor-pointer hover:bg-blue-400"
+                >
+                    Add
+                </button>
+            </form>
         </section>
     )
 }
