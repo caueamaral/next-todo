43442913c744f2c72c6cfb9536c72cc9d97286a8
Cauê Amaral <caue.amaral@hotmail.com>
2025-07-07 15:03:52 -0300

    fix: persist completed todos in sessionStorage

--- src/hooks/useToggleTodo.ts
+++ src/hooks/useToggleTodo.ts
@@ -1,19 +1,21 @@
 'use client'
 
 import { useTodoStore } from '../store/useTodoStore'
+import { TodoProps } from '../interfaces/TodoProps'
 
 export const useToggleTodo = () => {
     const todos = useTodoStore(state => state.todos)
     const setTodos = useTodoStore(state => state.setTodos)
 
     const toggleTodo = (id: number) => {
-        setTodos(
-            todos.map(todo =>
-                todo.id === id
-                    ? { ...todo, completed: !todo.completed }
-                    : todo
-            )
-        )
+        const newTodos:TodoProps[] = todos.map((todo:TodoProps) => {
+            if (todo.id === id) todo.completed = !todo.completed
+
+            return todo
+        })
+
+        setTodos(newTodos)
+        sessionStorage.setItem('todos', JSON.stringify([...newTodos]))
     }
 
     return { toggleTodo }
