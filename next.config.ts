19d38713ff3967c56909b23511475b0af0b3fb8b
Cauê Amaral <caue.amaral@hotmail.com>
2025-08-04 15:38:40 -0300

    fix(ci): add basePath and assetPrefix into next.config.js

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export', // Enable static export
+  basePath: '/next-todo',
+  assetPrefix: '/next-todo',
 };
 
 export default nextConfig;
