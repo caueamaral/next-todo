20c57a64d391d1de0039fafa5fc142f53168da20
Cauê Amaral <caue.amaral@hotmail.com>
2025-08-05 14:59:23 -0300

    fix: ensure generatedId function always returns 8 chars

--- src/functions/generateId.ts
+++ src/functions/generateId.ts
@@ -1,7 +1,7 @@
 export const generateId = (): number => {
     const randomNumber = Math.random()
     const randomAsString = randomNumber.toString()
-    const randomEdited = randomAsString.substring(1, 1).replace('0', '1') + randomAsString.substring(2, 10)
+    const randomEdited = randomAsString.substring(1, 1).replace('', '1') + randomAsString.substring(2, 10)
     const generatedId = Number(randomEdited)
 
     return generatedId
