2e1ffe635cd15c4aa93a8feaa99b544daa096044
Cauê Amaral <caue.amaral@hotmail.com>
2025-08-05 14:56:55 -0300

    fix(ci): relative path

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export', // Enable static export
   basePath: '/next-todo',
-  assetPrefix: '/next-todo',
+  assetPrefix: '/next-todo/',
   distDir: 'dist',
 };
 
